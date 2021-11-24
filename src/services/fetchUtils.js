ad26095d14bbecde4746248d30329d69fd8f4156
Paul Brubaker <pbrubak@gmail.com>
2021-11-23 19:48:09 -0800

    use proxy server to get around cors

--- src/services/fetchUtils.js
+++ src/services/fetchUtils.js
@@ -1,26 +1,35 @@
 /* eslint-disable indent */
 // Hi
 async function fetchRequest({ url, params, method, body, token }) {
-    if (params) {
-        url = new URL(url);
-        url.search = new URLSearchParams(params).toString();
-    }
+    // if (params) {
+    //     url = new URL(url);
+    //     url.search = new URLSearchParams(params).toString();
+    // }
 
-    const res = await fetch(url, {
-        method,
-        headers: token
-            ? {
-                  'Content-Type': 'application/json',
-                  Authorization: token,
-              }
-            : {
-                  'Content-Type': 'application/json',
-              },
-        body,
+    // const res = await fetch(url, {
+    //     method,
+    //     headers: token
+    //         ? {
+    //               'Content-Type': 'application/json',
+    //               Authorization: token,
+    //           }
+    //         : {
+    //               'Content-Type': 'application/json',
+    //           },
+    //     body,
+    // });
+
+    // const data = await res.json();
+    // return data;
+    const res = await fetch('https://proxy-fetch.herokuapp.com/', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url, params, method, body, token }),
     });
 
-    const data = await res.json();
-    return data;
+    return res.json();
 }
 
 export default fetchRequest;
